919b0eb5131f95b497a085b27862be27062b4086
Caleb Gasser <calebgasser@gmail.com>
2017-08-22 17:41:26 -0500

    Got movie and spotify search working

--- liri.js
+++ liri.js
@@ -1,8 +1,13 @@
 var keys = require('./keys');
 var request = require('request');
+var Spotify = require('node-spotify-api');
 
 var command = process.argv[2];
 var input = process.argv[3];
+var spotify = new Spotify({
+  id: '46a0a46dddc34fc8b0eee091681b5116',
+  secret: 'a340e7f621994effb5b8ba1b278f62bd'
+});
 
 var myTweets = function() {
   let baseUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json?';
@@ -34,7 +39,7 @@ var myTweets = function() {
         return;
       }
       messages = JSON.parse(body)
-      for(message in messages){
+      for (message in messages) {
         console.log(messages[message].created_at);
         console.log(messages[message].text + '\n\n');
       }
@@ -42,12 +47,62 @@ var myTweets = function() {
   })
 }
 
-var spotifyThisSong = function() {
-
+var spotifyThisSong = function(song) {
+  if (!song) {
+    song = 'The Sign'
+  }
+  spotify.search({
+    type: 'track',
+    query: song,
+    limit: 1
+  }).then(function(data) {
+    var trackData = data.tracks.items[0];
+    for (obj in trackData) {
+      if (obj === 'artists') {
+        for (artist in trackData[obj]) {
+          console.log('Artist: ' + trackData[obj][artist].name);
+        }
+      }
+      if (obj === 'name') {
+        console.log('Song Name: ' + trackData[obj]);
+      }
+      if (obj === 'album') {
+        console.log('Album: ' + trackData[obj].name);
+      }
+      if (obj === 'external_urls') {
+        console.log('Link: ' + trackData[obj].spotify);
+      }
+    }
+  }).catch(function(err) {
+    console.log(err);
+  })
 }
 
-var movieThis = function() {
-
+var movieThis = function(movie) {
+  if(!movie){
+    movie = 'Mr. Nobody';
+  }
+  request('http://www.omdbapi.com/?apikey=40e9cece&t=' + movie,function(err, res, body){
+    let data = JSON.parse(body);
+    if(data.Response !== 'False'){
+      console.log('Title: ' + data.Title);
+      console.log('Year: ' + data.Year);
+      for(rating in data.Ratings){
+        if(data.Ratings[rating].Source === 'Rotten Tomatoes'){
+          console.log('Rotten Tomatoes: ' + data.Ratings[rating].Value);
+        }
+        if(data.Ratings[rating].Source === 'Internet Movie Database'){
+          console.log('IMDB: ' + data.Ratings[rating].Value);
+        }
+      }
+      console.log('Country: ' + data.Country);
+      console.log('Language: ' + data.Language);
+      console.log('Plot: ' + data.Plot);
+      console.log('Actors: ' + data.Actors);
+    } else {
+      console.log(data.Error);
+    }
+  })
 }
 
 var doWhatItSays = function() {
@@ -59,13 +114,13 @@ switch (command) {
     myTweets();
     break;
   case 'spotify-this-song':
-    spotifyThisSong();
+    spotifyThisSong(input);
     break;
   case 'movie-this':
-    movieThis();
+    movieThis(input);
     break;
   case 'do-what-it-says':
-    doWhatItSays();
+    doWhatItSays(input);
     break;
   default:
     break;
